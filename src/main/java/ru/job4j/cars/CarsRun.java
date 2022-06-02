3e6da329282f844ad249d564dc8b160885b2fb4f
Denis Davydov <dvd9@yandex.ru>
2022-06-02 17:49:48 +0300

    2. Фильтры для площадок машин [#4745] fixed

--- src/main/java/ru/job4j/cars/CarsRun.java
+++ src/main/java/ru/job4j/cars/CarsRun.java
@@ -1,14 +1,13 @@
 package ru.job4j.cars;
 
-import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import ru.job4j.cars.model.Car;
+import ru.job4j.cars.persistence.AdRepository;
 
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
 public class CarsRun {
@@ -21,47 +20,10 @@ public class CarsRun {
                 .configure().build();
         try {
             SessionFactory sf = new MetadataSources(registry).buildMetadata().buildSessionFactory();
-            Session session = sf.openSession();
-            session.beginTransaction();
-/**
- *  объявления за последний день
- */
-            Date yesterday = new Date(System.currentTimeMillis() - 24 * 60 * 60 * 1000);
-            lastDayCars = session.createQuery(
-                    "select distinct c from Car c "
-                            + "join fetch c.engine "
-                            + "left join fetch c.photos p "
-                            + "where c.created >= :day", Car.class
-            )
-                    .setParameter("day", yesterday)
-                    .list();
-
-/**
- *  объявления только с фото
- */
-            carsWithPhotos = session.createQuery(
-                            "select distinct c from Car c "
-                                    + "join fetch c.engine "
-                                    + "join fetch c.carMark "
-                                    + "join fetch c.photos p ", Car.class
-                    )
-                    .list();
- /**
- *  только Audi
- */
-            audiCars = session.createQuery(
-                            "select distinct c from Car c "
-                                    + "join fetch c.engine "
-                                    + "join fetch c.carMark m "
-                                    + "left join c.photos p "
-                                    + "where m.name = :mark", Car.class
-                    )
-                    .setParameter("mark", "Audi")
-                    .list();
-
-            session.getTransaction().commit();
-            session.close();
-
+            AdRepository adRepository = new AdRepository(sf);
+            lastDayCars = adRepository.getLastDayCars();
+            carsWithPhotos = adRepository.getCarsWithPhoto();
+            audiCars = adRepository.getOnly("Audi");
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
--- src/main/java/ru/job4j/cars/persistence/AdRepository.java
+++ src/main/java/ru/job4j/cars/persistence/AdRepository.java
@@ -1,9 +1,77 @@
 package ru.job4j.cars.persistence;
 
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.springframework.stereotype.Repository;
+import ru.job4j.cars.model.Car;
+
+import java.util.Date;
+import java.util.List;
+import java.util.function.Function;
 
 @Repository
 public class AdRepository {
+    private final SessionFactory sf;
 
+    public AdRepository(SessionFactory sf) {
+        this.sf = sf;
+    }
+
+    private <T> T tx(final Function<Session, T> command) {
+        final Session session = sf.openSession();
+        final Transaction tx = session.beginTransaction();
+        try {
+            T rsl = command.apply(session);
+            tx.commit();
+            return rsl;
+        } catch (final Exception e) {
+            session.getTransaction().rollback();
+            e.printStackTrace();
+            return null;
+        } finally {
+            session.close();
+        }
+    }
+
+    public List<Car> getLastDayCars() {
+        Date yesterday = new Date(System.currentTimeMillis() - 24 * 60 * 60 * 1000);
+        return this.tx(
+            session -> session.createQuery(
+                "select distinct c from Car c "
+                + "join fetch c.engine "
+                + "left join fetch c.photos p "
+                + "where c.created >= :day", Car.class
+                )
+                .setParameter("day", yesterday)
+                .list()
+        );
+    }
+
+    public List<Car> getCarsWithPhoto() {
+        return this.tx(
+            session -> session.createQuery(
+                "select distinct c from Car c "
+                + "join fetch c.engine "
+                + "join fetch c.carMark "
+                + "join fetch c.photos p ", Car.class
+                )
+                .list()
+        );
+    }
+
+    public List<Car> getOnly(String carMark) {
+        return this.tx(
+            session -> session.createQuery(
+                "select distinct c from Car c "
+                + "join fetch c.engine "
+                + "join fetch c.carMark m "
+                + "left join c.photos p "
+                + "where m.name = :mark", Car.class
+                )
+                .setParameter("mark", carMark)
+                .list()
+        );
+    }
 }
